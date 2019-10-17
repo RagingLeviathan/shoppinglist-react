6c2d6d2929c2fbf75e295ec88aaacdf3d548563b
Nicole Graham <FieldsOfSeconds@hotmail.com>
2019-10-17 12:01:23 +0100

    Add files via upload

---
+++ src/components/navbar.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+//stateless functional component
+const NavBar = ({ totalCounters }) => {
+console.log("Nav bar - Rendered");
+
+    return ( <nav className="navbar navbar-light bg-light">
+        <a className="navbar-brand" href="#">
+            Navbar {" "}
+            <span className="badge badge-pill badge-secondary">     {totalCounters}
+            </span>
+        </a>
+      </nav> 
+    );
+};
+ 
+export default NavBar;
